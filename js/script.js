6d40690f195a2f3e81f45109178a35bbfb49d3da
Marie-Ehrman <marie.e.ehrman@gmail.com>
2019-07-23 17:54:39 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,16 +6,11 @@ project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+//declare variables like random quote and the quotes array
 
-var quotes = [
+let randomQuote = '';
+
+let quotes = [
   {
     quote: '"Not all those who wander are lost;"' ,
     source: 'J.R.R. Tolkien',
@@ -24,15 +19,21 @@ var quotes = [
   },
   {
     quote: '"...even if that were absolutely true, then I should one day like to show by my work what such an eccentric, such a nobody, has in his heart."',
-    source: 'Vincent Van Gogh'
+    source: 'Vincent Van Gogh',
+    citation: ' ',
+    year: 0
   },
   {
     quote: '"The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom."',
-    source: 'Isaac Asimov'
+    source: 'Isaac Asimov',
+    citation: ' ',
+    year: 0
   },
   {
     quote: '"In three words I can sum up everything I' + "'ve learned about life: it goes on." + '"',
-    source: 'Robert Frost'
+    source: 'Robert Frost',
+    citation:' ',
+    year: 0
   },
   {
     quote: '"Go placidly amid the noise and haste, and remember what peace there may be in silence."',
@@ -42,23 +43,25 @@ var quotes = [
   },
 ];
 
-for ( let i = 0; i < quotes.length; i +=1){
-quote = quotes[i].quote;
+//log test that array of quotes is functional and get it to display nicely
+
+for ( let i = 0; i < quotes.length; i += 1 ){
+randomQuote = quotes[i].quote;
 source = quotes[i].source;
 
-console.log(quote + "\n" + source);
+console.log(randomQuote + "\n" + source);
 }
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+
+
+//function to retrieve a quote from the position of randomly generated number
 
 function getRandomQuote(array) {
- let randomQuote = Math.random(Math.floor() * array.length) + 1;
-
-
+  let randomNumber = Math.floor(Math.random() * array.length);
+  console.log(randomNumber);
+  return array[randomNumber];
 }
+
+//console.log(getRandomQuote(quotes));
 
 /***
   Create the `printQuote` function to: 
@@ -72,9 +75,23 @@ function getRandomQuote(array) {
    - Don't forget to close that final `p` tag.
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
+ function printQuote(){
+  randomQuote = getRandomQuote(quotes);
+  console.log(randomQuote);
+  let html = ' ';
+  html = <p> + randomQuote.quote + "\n " + randomQuote.source + </p>;
+  if(randomQuote.citation != ' ') {
+    html += ", " + randomQuote.citation;
+  };
+  
+  if (randomQuote.year != 0){
+    html += ", " + randomQuote.year;
+  };
 
+  console.log(html);
+ }
 
-
+printQuote();
 
 /***
   When the "Show another quote" button is clicked, the event listener 
