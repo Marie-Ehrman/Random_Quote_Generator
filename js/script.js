da718e999b0866cbcd07bb7c56eb448e69922e55
Marie-Ehrman <marie.e.ehrman@gmail.com>
2019-07-29 18:04:26 -0700

    Unit 1 Project

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
 
 //declare variables like random quote and the quotes array
 
@@ -59,18 +57,6 @@ function getRandomQuote(array) {
 }
 
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
 
 //build print quote function
 
@@ -108,18 +94,8 @@ document.getElementById("quote-box").innerHTML = html;
 
  }
 
- //call the printQuote function
-
- printQuote();
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+//call the printQuote function when button is clicked
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
