495514753ed83f5dac1bc6a7d1fd782c57b1e062
Marie-Ehrman <marie.e.ehrman@gmail.com>
2019-07-31 13:30:40 -0700

    Unit 1 Project

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ let quotes = [
     source: 'J.R.R. Tolkien',
     citation: 'The Fellowship of the Ring',
     year: 1954
+
   },
   {
     quote: `...even if that were absolutely true, then I should one day like to show by my work what such an eccentric, such a nobody, has in his heart.`,
@@ -35,7 +36,34 @@ let quotes = [
     source: 'Max Ehrmann',
     citation: "Desiderata",
     year: 1952
+
   },
+  {
+    quote: `To live is the rarest thing in the world. Most people exist, that is all.`,
+    source: 'Oscar Wilde',
+
+  },
+  {
+    quote: `Nearly all men can stand adversity, but if you want to test a man's character, give him power.`,
+    source: 'Abraham Lincoln',
+
+  }, 
+  {
+    quote: `The secret to getting ahead is getting started.`,
+    source: 'Mark Twain',
+
+  },
+  {
+    quote: `The most common way people give up their power is by thinking they don’t have any.`,
+    source: 'Alice Walker',
+
+  },
+  {
+    quote: `If you’re not making mistakes, then you’re not making decisions.`,
+    source: 'Catherine Cook',
+
+  },
+  
 ];
 
 //log test that array of quotes is functional and get it to display nicely in console
@@ -56,6 +84,22 @@ function getRandomQuote(array) {
   return array[randomNumber];
 }
 
+/*try out function found in 
+https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+to randomly change background color on click of load quote.
+*/
+
+function randomBG() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var color = "rgb(" + x + "," + y + "," + z + ")";
+
+//log background color to the console to test the function  
+console.log(color);
+
+  document.body.style.background = color;
+  }
 
 
 //build print quote function
@@ -92,10 +136,16 @@ function getRandomQuote(array) {
   
 document.getElementById("quote-box").innerHTML = html;
 
+//call random backgorund function
+randomBG();
  }
 
+
+ 
 //call the printQuote function when button is clicked
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//set timer for screen to change, based on setInterval() MDN link provided in instructions
 
+let quoteTimer = window.setInterval(printQuote, 10000);
